60628ca0757fc4b3f09dc2c2fba581569bf17d50
Ovidiu Turcoane <ovidiuturcoane@yahoo.com>
2019-08-18 20:48:04 +0300

    State Input. Controlled component.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,14 @@ import React from 'react';
 import './App.css';
 import BasicButon from './js/BasicButton';
 import TextInput from "./js/TextInput";
+import StateInput from "./js/StateInput";
 
 function App() {
   return (
     <div className="App">
-      <BasicButon
-      />
+      <BasicButon/>
       <TextInput/>
+      <StateInput/>
     </div>
   );
 }
