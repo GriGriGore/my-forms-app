6ad30176527cf7a320b72eb7a9a3d19b0f0ac054
Ovidiu Turcoane <ovidiuturcoane@yahoo.com>
2019-09-01 13:58:22 +0300

    Async Course Component.

--- src/js/CourseAsyncFetch.js
+++ src/js/CourseAsyncFetch.js
@@ -1,94 +1,100 @@
 import React from 'react';
 import isEmail from 'validator/lib/isEmail';
-import FieldComponent from './FieldComponent';
+import FieldComponent from "./FieldComponent";
+import CourseSelect from "./CourseSelect";
 
-export default class FieldComponentValidation extends React.Component {
-    static displayName = 'field multi-input-validation';
+const Field = FieldComponent;
+
+export default class CourseAsyncFetch extends React.Component {
+    static displayName = 'course-sync-fetch';
     state = {
         fields: {
             name: '',
             email: '',
+            course: null,
+            department: null,
         },
         fieldErrors: {},
         people: [],
     };
-    onInputChange = ({name, value, error}) => {
-        const fields = this.state.fields;
-        const fieldErrors = this.state.fieldErrors;
 
-        fields[name] = value;
-        fieldErrors[name] = error;
-        this.setState({fields, fieldErrors});
-    };
     onFormSubmit = (evt) => {
         const people = this.state.people;
         const person = this.state.fields;
 
         evt.preventDefault();
 
-        if (this.validate()) {
-            return;
-        }
+        if (this.validate()) return;
 
         this.setState({
             people: people.concat(person),
             fields: {
                 name: '',
                 email: '',
+                course: null,
+                department: null,
             },
         });
+    };
 
-        people.map((p, idx) =>
-            console.log(`${p.name} (${p.email}) - ${idx}`));
+    onInputChange = ({name, value, error}) => {
+        const fields = this.state.fields;
+        const fieldErrors = this.state.fieldErrors;
+
+        fields[name] = value;
+        fieldErrors[name] = error;
+
+        this.setState({fields, fieldErrors});
     };
 
     validate = () => {
         const person = this.state.fields;
         const fieldErrors = this.state.fieldErrors;
-        const errMessages = Object.keys(fieldErrors)
-            .filter((k) => fieldErrors[k]);
-
-        if(!person.name) return true;
-        if(!person.email) return true;
-        if(errMessages.length) return true;
-
-        return false;
+        const errMessage = Object.keys(fieldErrors).filter(
+            (key) => fieldErrors[key]
+        );
+        if (!person.name) return true;
+        if (!person.email) return true;
+        if (!person.course) return true;
+        if (!person.department) return true;
+        if (errMessage.length) return true;
     };
 
     render() {
         return (
             <div>
-                <h1>Stateful Sign up Multi Sheet with Field Validation</h1>
+                <h1>Course Sync Fetch</h1>
                 <form onSubmit={this.onFormSubmit}>
-                    <FieldComponent
+                    <Field
                         placeholder='Name'
                         name='name'
                         value={this.state.fields.name}
                         onChange={this.onInputChange}
                         validate={(val) => (val ? false : 'Name required')}
                     />
-                    <span style={{color: 'red'}}>{this.state.fieldErrors.name}</span>
-
                     <br/>
-
-                    <FieldComponent
+                    <Field
                         placeholder='Email'
                         name='email'
                         value={this.state.fields.email}
                         onChange={this.onInputChange}
                         validate={(val) => (isEmail(val) ? false : 'Invalid email')}
-                    />
-                    <span style={{color: 'red'}}>{this.state.fieldErrors.email}</span>
-
+                    >
+                    </Field>
                     <br/>
-
+                    <CourseSelect
+                        department={this.state.fields.department}
+                        course={this.state.fields.course}
+                        onChange={this.onInputChange}
+                    />
                     <input type='submit' disabled={this.validate()}/>
                 </form>
                 <div>
                     <h3>People</h3>
                     <ul>
-                        {this.state.people.map(({name, email}, idx) =>
-                            <li key={idx}>{name} ({email})</li>)}
+                        {this.state.people.map(({name, email, department, course}, i) =>
+                            <li key={i}>{[name, email, department, course].join(' - ')}</li>)
+                        }
                     </ul>
                 </div>
             </div>
